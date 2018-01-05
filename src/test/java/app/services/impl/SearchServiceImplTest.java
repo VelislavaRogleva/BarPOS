3cd088cab81a3bb420a6c1a257ba817adbb64450
komitoff <todorkomitov@gmail.com>
2018-01-05 18:04:02 +0200

    Added SearchServiceImplTest

---
+++ src/test/java/app/services/impl/SearchServiceImplTest.java
@@ -0,0 +1,20 @@
+package app.services.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SearchServiceImplTest {
+
+    @Test
+    public void findProductsByName() {
+    }
+
+    @Test
+    public void findCategoriesByName() {
+    }
+
+    @Test
+    public void findUsersByName() {
+    }
+}
